c3f4dd57c12b7b333e000903b448ec972f63d4f0
Funii4 <79381109+Funii4@users.noreply.github.com>
2021-09-05 19:01:00 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_scripts {
+    'clientconfig.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'serverconfig.lua',
+    'server.lua',
+    "@mysql-async/lib/MySQL.lua"
+}
---
+++ clientconfig.lua
@@ -0,0 +1,52 @@
+--[[  
+░█─░█ ░█▄─░█ ░█─▄▀ ░█──░█ 　 ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ▀█▀ ░█▀▀█ ░█─░█ ░█▀▀▀ ─█▀▀█ ▀▀█▀▀ 　 ░█──░█ █▀█ 
+░█─░█ ░█░█░█ ░█▀▄─ ░█░█░█ 　 ░█▄▄█ ░█░█░█ ─░█── ░█─ ░█─── ░█▀▀█ ░█▀▀▀ ░█▄▄█ ─░█── 　 ─░█░█─ ─▄▀ 
+─▀▄▄▀ ░█──▀█ ░█─░█ ░█▄▀▄█ 　 ░█─░█ ░█──▀█ ─░█── ▄█▄ ░█▄▄█ ░█─░█ ░█▄▄▄ ░█─░█ ─░█── 　 ──▀▄▀─ █▄▄
+░█████╗░██╗░░░░░██╗███████╗███╗░░██╗████████╗  ░█████╗░░█████╗░███╗░░██╗███████╗██╗░██████╗░
+██╔══██╗██║░░░░░██║██╔════╝████╗░██║╚══██╔══╝  ██╔══██╗██╔══██╗████╗░██║██╔════╝██║██╔════╝░
+██║░░╚═╝██║░░░░░██║█████╗░░██╔██╗██║░░░██║░░░  ██║░░╚═╝██║░░██║██╔██╗██║█████╗░░██║██║░░██╗░
+██║░░██╗██║░░░░░██║██╔══╝░░██║╚████║░░░██║░░░  ██║░░██╗██║░░██║██║╚████║██╔══╝░░██║██║░░╚██╗
+╚█████╔╝███████╗██║███████╗██║░╚███║░░░██║░░░  ╚█████╔╝╚█████╔╝██║░╚███║██║░░░░░██║╚██████╔╝
+░╚════╝░╚══════╝╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░  ░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░░░░╚═╝░╚═════╝░]]
+unkw = {}
+unkw.weaponblacklisting = true
+--// Blacklistované zbraně \\-- 
+
+unkw.zbrane = {
+    "WEAPON_COMBATMG",
+    "WEAPON_COMBATMG_MK2",
+    "WEAPON_COMPACTLAUNCHER",
+    "WEAPON_FIREWORK",
+    "WEAPON_GRENADE",
+    "WEAPON_GRENADELAUNCHER",
+    "WEAPON_GRENADELAUNCHER_SMOKE",
+    "WEAPON_HEAVYPISTOL",
+    "WEAPON_HEAVYSHOTGUN",
+    "WEAPON_HEAVYSNIPER",
+    "WEAPON_HEAVYSNIPER_MK2",
+    "WEAPON_HOMINGLAUNCHER",
+    "WEAPON_MG",
+    "WEAPON_MARKSMANPISTOL",
+    "WEAPON_MARKSMANRIFLE",
+    "WEAPON_MARKSMANRIFLE_MK2",
+    "WEAPON_MINIGUN",
+    "WEAPON_MOLOTOV",
+    "WEAPON_PIPEBOMB",
+    "WEAPON_PISTOL_MK2",
+    "WEAPON_PROXMINE",
+    "WEAPON_RPG",
+    "WEAPON_RAILGUN",
+    "WEAPON_REVOLVER_MK2",
+    "WEAPON_SMG_MK2",
+    "WEAPON_SNSPISTOL_MK2",
+    "WEAPON_SMOKEGRENADE",
+    "WEAPON_SNIPERIFLE",
+    "WEAPON_SPECIALCARBINE",
+    "WEAPON_SPECIALCARBINE_MK2",
+    "WEAPON_SWEEPERSHOTGUN",
+    "WEAPON_RAYPISTOL",
+    "WEAPON_RAYCARBINE",
+    "WEAPON_RAYMINIGUN",
+    "WEAPON_STONE_HATCHET",
+    }
+    
